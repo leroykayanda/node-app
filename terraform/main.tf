6a9ecc5608da42e94973a847f2831a49a19958ad
Leroy Kayanda <leroykayanda@Leroys-MacBook-Pro.local>
2023-11-17 06:59:28 +0300

    testing pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 module "ecr_repo" {
-  source            = "./terraform/modules/aws-ecr-repo"
+  source            = "terraform/modules/aws-ecr-repo"
   env               = var.env
   microservice_name = var.service
 }
